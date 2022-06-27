e2f0e64de2e42665b2b7a89caca854859c2c8994
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2022-06-27 11:37:08 -0500

    feat: add test modules with a simple util class

---
+++ src/app/Utils.ts
@@ -0,0 +1,13 @@
+import { parse, UrlWithParsedQuery } from "url";
+
+export class Utils {
+
+
+    public static parseUrl(url: string): UrlWithParsedQuery {
+        return parse(url, true);
+    }
+
+    public static toUpperCase(args: string): string {
+        return args.toUpperCase();
+    }
+}
---
+++ src/test/Utils.test.ts
@@ -0,0 +1,23 @@
+import { Utils } from "../app/Utils";
+
+describe('Utils', () => {
+    test('should uppercase test', () => {
+        const uppercaseString = Utils.toUpperCase('abc');
+        expect(uppercaseString).toBe('ABC');
+    });
+    test('should parse url', () => {
+        const parseUrl = Utils.parseUrl('http://localhost:8080/login');
+        expect(parseUrl.href).toBe('http://localhost:8080/login');
+        expect(parseUrl.port).toBe('8080');
+        expect(parseUrl.protocol).toBe('http:');
+        expect(parseUrl.query).toEqual({});
+    });
+    test('should url with query params', () => {
+        const parseUrl = Utils.parseUrl('http://localhost:8080/login?user=user&password=pass');
+        const expectedQuery = {
+            user: 'user',
+            password: 'pass'
+        };
+        expect(parseUrl.query).toEqual(expectedQuery);
+    });
+});
